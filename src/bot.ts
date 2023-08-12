ad6a4cf23e2b0a14382cca90ca2a7b3dbe1c5695
Riley Calhoun <rileycalhoun0228@gmail.com>
2023-08-12 13:03:00 -0700

    Nicknames

--- src/bot.ts
+++ src/bot.ts
@@ -3,7 +3,19 @@ import Guilds from './models/Guild';
 import { utcToZonedTime } from 'date-fns-tz';
 import * as mongo from './mongo';
 
-const { BOT_TOKEN } = process.env;
+const { BOT_TOKEN, BOT_ID } = process.env;
+
+if(!BOT_TOKEN) {
+    console.log('No bot token provided!');
+    process.exit(0);
+}
+
+if(!BOT_ID) {
+    console.log('No bot ID provided!');
+    process.exit(0);
+}
+
+
 
 const client = new Client({
     intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages]
@@ -12,6 +24,13 @@ const client = new Client({
 const sendMessages = async () => {
     const allGuilds = await Guilds.find({});
     allGuilds.forEach(guild => {
+        const nickname = guild.nickname;
+        if(nickname) {
+            let self = client.guilds.cache.get(guild.id)
+                ?.members.cache.get(BOT_ID);
+            self?.setNickname(nickname as string);
+        }
+
         const currentTime = new Date()
             .toLocaleTimeString('en-US', {
                 timeZone: guild.timezone as string,
@@ -52,5 +71,5 @@ client.on('guildCreate', guild => mongo.createIfAbsent(guild));
 
 client.on('ready', () => {
     console.log('Bot is ready!');
-    setInterval(async () => sendMessages(), 60000);
+    setInterval(async () => sendMessages(), 10000);
 });
