0f799b9aea7d0d5c68afbbc08f1448bc6696897a
Riley Calhoun <rileycalhoun0228@gmail.com>
2023-08-19 11:21:54 -0700

    Normalize code

--- src/bot.ts
+++ src/bot.ts
@@ -5,23 +5,24 @@ import * as mongo from './mongo';
 
 const { BOT_TOKEN, BOT_ID, DASHBOARD_URI } = process.env;
 
-if(!BOT_TOKEN) {
-    console.log('No bot token provided!');
-    process.exit(0);
-}
-
-if(!BOT_ID) {
-    console.log('No bot ID provided!');
-    process.exit(0);
-}
-
 mongo.connect();
-
 const client = new Client({
     intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages]
 });
 
-const setStatus = async () => {
+async function checkEnvironment() {
+    if(!BOT_TOKEN) {
+        console.log('No bot token provided!');
+        process.exit(0);
+    }
+    
+    if(!BOT_ID) {
+        console.log('No bot ID provided!');
+        process.exit(0);
+    }
+}
+
+async function updateBotStatus() {
     const guildCount = client.guilds.cache.size;
     client.user?.setPresence({
         status: 'online',
@@ -33,17 +34,17 @@ const setStatus = async () => {
     })
 }
 
-const checkNicknames = async () => {
+async function syncDatabaseSettings() {
     const allGuilds = await Guilds.find({});
     allGuilds.forEach(guild => {
         const nickname = guild.nickname;
         let self = client.guilds.cache.get(guild.id)
-                ?.members.cache.get(BOT_ID);
+                ?.members.cache.get(BOT_ID as string);
         self?.setNickname(nickname as string);
     });
 }
 
-const sendMessages = async () => {
+async function sendGuildMessages() {
     const allGuilds = await Guilds.find({});
     allGuilds.forEach(guild => {
         const currentTime = new Date()
@@ -63,8 +64,14 @@ const sendMessages = async () => {
         if (currentTime !== guild.messageTime) return;
 
         const channel = currentGuild.channels.cache.get(guild.channel as string);
-        if (!channel) return;
-        if (!channel.isTextBased()) return;
+        
+        if (!channel) {
+            return;
+        }
+
+        if (!channel.isTextBased()) {
+            return;
+        }
 
         const zonedData = utcToZonedTime(new Date(), guild.timezone as string);
         const day = zonedData.getDay();
@@ -78,15 +85,21 @@ const sendMessages = async () => {
     });
 }
 
-client.on('guildAvailable', guild => mongo.createIfAbsent(guild));
-client.on('guildCreate', guild => mongo.createIfAbsent(guild));
+client.on('guildAvailable', (guild) => mongo.createIfAbsent(guild));
+client.on('guildCreate', (guild) => mongo.createIfAbsent(guild));
+client.on('ready', onReady);
 
-client.on('ready', () => {
-    setInterval(async () => sendMessages(), 60_000);
-    setInterval(async () => checkNicknames(), 10_000);
-    setInterval(async () => setStatus(), 60_000);
+function onReady() {
+    setInterval(syncDatabaseSettings, 10_000);
+    setInterval(sendGuildMessages, 60_000);
+    setInterval(updateBotStatus, 60_000);
 
     console.log(`Shard ${client.shard?.ids} is ready!`);
-});
+}
 
-client.login(BOT_TOKEN);
+async function start() {
+    await checkEnvironment();
+    await client.login(BOT_TOKEN);
+}
+
+start();
